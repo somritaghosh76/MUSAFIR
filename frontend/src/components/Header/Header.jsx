33fba682a939ae994fa70824f85a4d402e673803
Kanish Mittal <mittalkanish21@gmail.com>
2025-01-25 02:13:34 +0530

    frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+// import { Container, Row, Button } from 'react-bootstrap';
+import { Container, Row, Button } from 'reactstrap';
+import { NavLink, Link } from "react-router-dom";
+
+import logo from "../../assets/images/logo.png";
+import './header.css';
+
+const nav__links = [
+    {
+        path: "/home",
+        display: "Home"
+    },
+    {
+        path: "/about",
+        display: "About"
+    },
+    {
+        path: "/tours",
+        display: "Tours"
+    },
+]
 
 const Header = () => {
-    return <div>Header</div>;
+    return <header className="header">
+        <Container>
+            <Row>
+                <div className="nav__wrapper d-flex justify-content-between align-items-center">
+                    {/* ====== logo ====== */}
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+                    {/* ====== logo end====== */}
+
+                    {/* ====== menu start====== */}
+                    <div className="navigation">
+                        <ul className="menu d-flex align-items-center gap-5">
+                            {
+                                nav__links.map((items, index) => (
+                                    <li className="nav__item" key={index}>
+                                        <NavLink to={items.path}
+                                            className={
+                                                navClass =>
+                                                    navClass.isActive ? "active__link" : ""}>{items.display}
+                                        </NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    {/* ====== menu end====== */}
+
+                    <div className="nav__right d-flex align-items-center gap-4">
+                        <div className="nav__btns d-flex align-items-center gap-4">
+                            <Button className="btn seconday__btn">
+                                <Link to='/login'>Login</Link>
+                            </Button>
+                            <Button className="btn primary__btn">
+                                <Link to='/register'>Register</Link>
+                            </Button>
+                        </div>
+                        <span className="mobile__menu">
+                            <i class="ri-menu-line"></i>
+                        </span>
+                    </div>
+                </div>
+            </Row>
+        </Container>
+    </header>;
 };
 
 export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import '../styles/home.css';
+import { Container, Row, Col } from 'reactstrap';
+import worldImg from '../assets/images/world.png';
+import heroImg from '../assets/images/hero-img01.jpg';
+import heroImg02 from '../assets/images/hero-img02.jpg';
+import heroVideo from '../assets/images/hero-video.mp4';
+import Subtitle from './../shared/Subtitle';
 
 const Home = () => {
-    return <div>Home</div>;
+    return <>
+
+        {/* ======= hero section start ======= */}
+        <section>
+            <Container>
+                <Row>
+                    <Col lg='6'>
+                        <div className="hero__content">
+                            <div className="hero__subtitle d-flex align-items-centre">
+                                <Subtitle subtitle={'Know Before You Go'} />
+                                <img src={worldImg} alt="" />
+                            </div>
+                            <h1>Travelling opens the dooor to creating{" "} <span className="highlight">
+                                memories</span></h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus temporibus libero, voluptas, laboriosam quos impedit incidunt ratione corporis dolorem natus eum eius! Vel?</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+        {/* ======= hero section end ======= */}
+    </>
 };
 
 export default Home;
