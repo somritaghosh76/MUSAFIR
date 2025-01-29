8afe86363e575a1e87aa12e98682a7d8b4954537
Kanish Mittal <mittalkanish21@gmail.com>
2025-01-30 01:32:39 +0530

    frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,58 +9,78 @@ import Subtitle from './../shared/Subtitle';
 
 import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
+import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
 
 const Home = () => {
-    return <>
+    return (
+        <>
 
-        {/* ======= hero section start ======= */}
-        <section>
-            <Container>
-                <Row>
-                    <Col lg='6'>
-                        <div className="hero__content">
-                            <div className="hero__subtitle d-flex align-items-centre">
-                                <Subtitle subtitle={'Know Before You Go'} />
-                                <img src={worldImg} alt="" />
+            {/* ======= hero section start ======= */}
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='6'>
+                            <div className="hero__content">
+                                <div className="hero__subtitle d-flex align-items-centre">
+                                    <Subtitle subtitle={'Know Before You Go'} />
+                                    <img src={worldImg} alt="" />
+                                </div>
+                                <h1>Travelling opens the dooor to creating{" "} <span className="highlight">
+                                    memories</span></h1>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus temporibus libero, voluptas, laboriosam quos impedit incidunt ratione corporis dolorem natus eum eius! Vel?</p>
                             </div>
-                            <h1>Travelling opens the dooor to creating{" "} <span className="highlight">
-                                memories</span></h1>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus temporibus libero, voluptas, laboriosam quos impedit incidunt ratione corporis dolorem natus eum eius! Vel?</p>
-                        </div>
-                    </Col>
-                    <Col lg='2'>
-                        <div className="hero__img-box">
-                            <img src={heroImg} alt="" />
-                        </div>
-                    </Col>
-                    <Col lg='2'>
-                        <div className="hero__img-box mt-4">
-                            <video src={heroVideo} alt="" autoPlay loop muted/>
-                        </div>
-                    </Col>
-                    <Col lg='2'>
-                        <div className="hero__img-box mt-5">
-                            <img src={heroImg02} alt="" />
-                        </div>
-                    </Col>
+                        </Col>
+                        <Col lg='2'>
+                            <div className="hero__img-box">
+                                <img src={heroImg} alt="" />
+                            </div>
+                        </Col>
+                        <Col lg='2'>
+                            <div className="hero__img-box mt-4">
+                                <video src={heroVideo} alt="" autoPlay loop muted />
+                            </div>
+                        </Col>
+                        <Col lg='2'>
+                            <div className="hero__img-box mt-5">
+                                <img src={heroImg02} alt="" />
+                            </div>
+                        </Col>
 
-                    <SearchBar />
-                </Row>
-            </Container>
-        </section>
-        {/* ======= hero section end ======= */}
-        <section>
-            <Container>
-                <Row>
-                    <Col lg='3'>
-                    <h5 className="services__subtitle">What we serve</h5>
-                    <h2 className="services__title">We offer our best services</h2>
-                    </Col>
-                    <ServiceList />
-                </Row>
-            </Container>
-        </section>
-    </>
+                        <SearchBar />
+                    </Row>
+                </Container>
+            </section>
+            {/* ======= hero section end ======= */}
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='3'>
+                            <h5 className="services__subtitle">What we serve</h5>
+                            <h2 className="services__title">We offer our best services</h2>
+                        </Col>
+                        <ServiceList />
+                    </Row>
+                </Container>
+            </section>
+
+            {/* ======= featured tour section start ======= */}
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="12" className="mb-5" >                                
+                            <Subtitle subtitle={'Explore'} />
+                            <h2 className="featured__tour-title">Our Featured Tours</h2>
+                        </Col>
+                        <FeaturedTourList/>
+                    </Row>
+                </Container>
+            </section>
+            {/* ======= featured tour section end ======= */}
+            {/* ======= experience section start ======= */}
+            
+            {/* ======= experience section end ======= */}
+        </>
+    );
 };
 
 export default Home;
---
+++ frontend/src/utils/avgRating.js
@@ -0,0 +1,19 @@
+
+
+const calculateAvgRating = reviews => {
+
+    const totalRating = reviews?.reduce((acc, items) => acc + items.rating, 0);
+    const avgRating =
+        totalRating === 0
+            ? " "
+            : totalRating === 1
+            ? totalRating
+            : (totalRating / reviews?.length).toFixed(1);
+
+return{
+    totalRating,
+    avgRating
+}
+}
+
+export default calculateAvgRating;
