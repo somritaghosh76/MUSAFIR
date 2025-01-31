24e446ebbeff32886c33cb167edd17c5b15ab891
Kanish Mittal <mittalkanish21@gmail.com>
2025-02-01 02:47:52 +0530

    frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-// import { Container, Row, Button } from 'react-bootstrap';
+import React,{useRef, useEffect} from "react";
 import { Container, Row, Button } from 'reactstrap';
 import { NavLink, Link } from "react-router-dom";
 
@@ -22,7 +21,26 @@ const nav__links = [
 ];
 
 const Header = () => {
-    return <header className="header">
+
+    const headerRef = useRef(null)
+
+    const stickyHeaderFunc = ()=> {
+        window.addEventListener('scroll', ()=>{
+            if(document.body.scrollTop >80 || document.documentElement.scrollTop >80){
+                headerRef.current.classList.add('sticky__header')
+            }else{
+                headerRef.current.classList.remove('sticky__header')
+            }
+        })
+    }
+
+    useEffect(()=>{
+        stickyHeaderFunc()
+
+        return window.removeEventListener('scroll', stickyHeaderFunc)
+    })
+
+    return <header className="header" ref={headerRef}>
         <Container>
             <Row>
                 <div className="nav__wrapper d-flex justify-content-between align-items-center">
@@ -36,12 +54,12 @@ const Header = () => {
                     <div className="navigation">
                         <ul className="menu d-flex align-items-center gap-5">
                             {
-                                nav__links.map((items, index) => (
+                                nav__links.map((item, index) => (
                                     <li className="nav__item" key={index}>
-                                        <NavLink to={items.path}
+                                        <NavLink to={item.path}
                                             className={
                                                 navClass =>
-                                                    navClass.isActive ? "active__link" : ""}>{items.display}
+                                                    navClass.isActive ? "active__link" : ""}>{item.display}
                                         </NavLink>
                                     </li>
                                 ))
@@ -52,13 +70,14 @@ const Header = () => {
 
                     <div className="nav__right d-flex align-items-center gap-4">
                         <div className="nav__btns d-flex align-items-center gap-4">
-                            <Button className="btn seconday__btn">
+                            <Button className="btn secondary__btn">
                                 <Link to='/login'>Login</Link>
                             </Button>
                             <Button className="btn primary__btn">
                                 <Link to='/register'>Register</Link>
                             </Button>
                         </div>
+
                         <span className="mobile__menu">
                             <i class="ri-menu-line"></i>
                         </span>
