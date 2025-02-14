267b82c9fb3c4ddd0e6a76c4c3e3040f089339fd
Kanish Mittal <mittalkanish21@gmail.com>
2025-02-14 22:12:27 +0530

    backend

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -1,17 +1,153 @@
 import Tour from '../models/Tour.js'
 
 //create new tour
-export const createTour = async (req,res)=>{
-
+export const createTour = async (req, res) => {
     const newTour = new Tour(req.body)
-
-    try{
+    try {
         const savedTour = await newTour.save()
+        res.status(200).json({
+            success: true,
+            message: 'Successfully created',
+            data: savedTour,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to create, try again'
+        });
+    }
+};
 
-        res.status(200).json({success:true, message:'Successfully created', data:savedTour})
+//update tour
+export const updateTour = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const updatedTour = await Tour.findByIdAndUpdate(id, {
+            $set: req.body
+        }, { new: true });
+        res.status(200).json({
+            success: true,
+            message: 'Successfully updated',
+            data: updatedTour,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to update, try again'
+        });
+    }
+};
 
-    } catch (err){
-        res.status(500).json({success:false, message:'Failed to create, try again'})
+//delete tour
+export const deleteTour = async (req, res) => {
+    const id = req.params.id;
+    try {
+        await Tour.findByIdAndDelete(id);
+        res.status(200).json({
+            success: true,
+            message: 'Successfully deleted',
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to delete, try again'
+        });
+    }
+};
 
+//getSingle tour
+export const getSingleTour = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const tour = await Tour.findById(id);
+        if (!tour) {
+            return res.status(404).json({
+                success: false,
+                message: "Tour not found",
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: 'Successfully fetched',
+            data: tour,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+//get all tours
+export const getAllTour = async (req, res) => {
+    const page = parseInt(req.query.page) || 0;
+    try {
+        const tours = await Tour.find({}).skip(page * 8).limit(8);
+        res.status(200).json({
+            success: true,
+            count: tours.length,
+            message: 'Successfully fetched',
+            data: tours,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+//Get Tour By Search
+export const getTourBySearch = async (req, res) => {
+    const city = new RegExp(req.query.city, 'i'); //here 'i' means case sensitive
+    const distance = parseInt(req.query.distance) || 0; 
+    const maxGroupSize = parseInt(req.query.maxGroupSize) || 0;
+
+    try {
+        //gte means greter than equal to
+        const tours = await Tour.find({
+            city,
+            distance: { $gte: distance },
+            maxGroupSize: { $gte: maxGroupSize },
+        });
+
+        res.status(200).json({
+            success: true,
+            message: 'Successfully fetched',
+            data: tours,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+//get featured tours
+export const getFeaturedTour = async (req, res) => {
+    try {
+        const tours = await Tour.find({featured:true}).limit(8);
+        res.status(200).json({
+            success: true,
+            message: 'Successfully fetched',
+            data: tours,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+//get tour counts
+export const getTourCount = async (req,res) => {
+    try {
+        const tourCount = await Tour.estimatedDocumentCount();
+        res.status(200).json({success:true, data:tourCount});
+    } catch (err) {
+        res.status(500).json({success:false, message: "Failed to fetch!" });
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import tourRoute from './routes/tours.js'
 
 
 dotenv.config()
@@ -29,6 +30,7 @@ const connect = async()=>{
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
+app.use('/tours', tourRoute )
 
 app.listen(port, ()=>{
     connect();
